59797ce74a43cb9a7db3c6a36767a433fd00601d
Sergio Trett <serge3ling@gmail.com>
2020-12-05 16:11:08 +0200

    Minimal project.

---
+++ src/main/java/y3practice2river/Main.java
@@ -0,0 +1,12 @@
+package y3practice2river;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("y3practice2river");
+    }
+    
+}
