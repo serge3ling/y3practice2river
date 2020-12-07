866b2f9860b0542ca49623dda70539c477634da8
alex3 <alex3@localhost>
2020-12-07 14:22:57 +0200

    River

---
+++ src/main/java/y3practice2river/River.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package y3practice2river;
+
+/**
+ *
+ * @author alex3
+ */
+public class River {
+    int length;
+    int depth;
+    String name;
+    int discharge;
+    String continent;
+    int basin;
+    public int periodDischarge(String start,String end){
+    return 0;
+    };
+ 
+    
+}
